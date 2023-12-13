d4214103cc73426ab901b643dcc08c487cbf3492
Steven Charles <iwritecodes@protonmail.com>
2023-12-12 19:12:13 -0500

    Update Bid

--- tests/bids.test.js
+++ tests/bids.test.js
@@ -4,60 +4,63 @@ const { PrismaClient } = require("@prisma/client");
 
 // Mock the Prisma client
 jest.mock("@prisma/client", () => {
-  // Define the mock implementation within the factory function
-  const mockDeliveryRequest = {
-    id: 1,
-    // other properties as per your schema
-  };
+    const mockDeliveryRequest = {
+      id: 1,
+      // other properties as per your schema
+    };
+  
+    const mockBid = {
+      id: 1,
+      driver_id: 1,
+      delivery_request_id: 1,
+      bid_price: 100,
+      // other properties as per your schema
+    };
+  
+    return {
+      PrismaClient: jest.fn().mockImplementation(() => ({
+        deliveryRequest: {
+          update: jest.fn().mockResolvedValue(mockDeliveryRequest),
+        },
+        bid: {
+          create: jest.fn().mockResolvedValue(mockBid),
+          findUnique: jest.fn().mockResolvedValue(mockBid), // Mock for findUnique method
+          update: jest.fn().mockResolvedValue({ ...mockBid, bid_price: 200 }), // Mock for update method
+        },
+      })),
+    };
+  });
 
-  const mockBid = {
-    id: 1,
-    driver_id: 1,
-    delivery_request_id: 1,
-    bid_price: 100,
-  };
-
-  return {
-    PrismaClient: jest.fn().mockImplementation(() => ({
-      deliveryRequest: {
-        update: jest.fn().mockResolvedValue(mockDeliveryRequest),
-      },
-      bid: {
-        create: jest.fn().mockResolvedValue(mockBid),
-      },
-    })),
-  };
-});
-
-describe("/record-bid", () => {
   let server, request;
 
-  beforeAll(() => {
-    server = app.listen(); // Start your app on a random free port
-    request = supertest(server);
+  describe("/record-bid", () => {
+    
+  
+    beforeAll(() => {
+      server = app.listen(); // Start your app on a random free port
+      request = supertest(server);
+    });
+  
+    afterAll(() => {
+      server.close(); // Close the server after the tests
+    });
+  
+    it("should record a bid successfully", async () => {
+      const mockBidRequest = {
+        deliveryRequestId: 1,
+        driverId: 1,
+        bidPrice: 100,
+      };
+  
+      const response = await request.post("/bid/record-bid").send(mockBidRequest);
+  
+      expect(response.status).toBe(200);
+      expect(response.body.success).toBe(true);
+      expect(response.body.message).toBe("Bid recorded successfully.");
+    });
   });
-
-  afterAll(() => {
-    server.close(); // Close the server after the tests
-  });
-
-  it("should record a bid successfully", async () => {
-    const mockBidRequest = {
-      deliveryRequestId: 1,
-      driverId: 1,
-      bidPrice: 100,
-    };
-
-    const response = await request.post("/bid/record-bid").send(mockBidRequest);
-
-    expect(response.status).toBe(200);
-    expect(response.body.success).toBe(true);
-    expect(response.body.message).toBe("Bid recorded successfully.");
-
-  });
-});
-
-describe('/update-bid', () => {
+  
+  describe('/update-bid', () => {
     it('should update a bid successfully', async () => {
       const mockBidUpdateRequest = {
         bidId: 1,
@@ -71,7 +74,8 @@ describe('/update-bid', () => {
       expect(response.body.success).toBe(true);
       expect(response.body.message).toBe('Bid updated successfully.');
   
+      // Additional assertions can be added as needed
     });
   
-
+    // Additional test cases can be added here
   });
